72691086814820f657a16f210d4ee1016518d28a
deepuuu2529 <deepuuu8112@icloud.com>
2025-02-26 16:20:47 +0530

    Declaration and Initialisation of 1D array

---
+++ Arrays/Array.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        //declaration and initialisation of Array 1D
+        // syntax
+        /*
+        <datatype>[] <arrayname> = new <datatype>[]{<elements>};
+        ex:
+         */
+        int[] arr = new int[]{1,2,3,4,5,6,7};
+
+        //declaration of the array elements
+
+        // accessing array elements
+        /*
+        <arrayname>[<index]
+        ex:
+         */
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+
+    }
+}
